7db4f308e1793d6be7b7e20052a7dd9651551043
Your Name~ <harish.s@google.com>
2024-01-11 16:02:39 +0530

    Complete

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,71 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(){
+        super();
+        this.state={
+            imageIndex:0
+        }
+    }
+  render(){
+    let indexZero=()=>{
+        this.setState({
+            imageIndex:0
+        })
+    }
+    let indexIncrement=()=>{
+        this.setState({
+            imageIndex:this.state.imageIndex+1
+        })
+    }
+    let indexDecrement=()=>{
+        this.setState({
+            imageIndex:this.state.imageIndex-1
+        })
+    }
+    let indexLast=()=>{
+        this.setState({
+            imageIndex:2
+        })
+    }
+    let prevImage=()=>{
+        this.state.imageIndex-1<0?indexLast():indexDecrement();
+    }
+    let nextImage=()=>{
+            this.state.imageIndex+1>images.length-1?indexZero():indexIncrement();
+    }
+    const myStyle ={
+        backgroundImage:`url(${images[this.state.imageIndex].img})`,
+        backgroundSize: "fit",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize:"100% 100%",
+        height: "80vh",
+        width: "42vw",
+    }
+    return(
+        <div className="main-container" style={myStyle}>
+            <div className="arrow">
+                <ArrowBackIosIcon onClick={prevImage}/>
+            </div>
+            <div className="Sub-container">
+                <div className="Content">
+                    <h1>
+                        {images[this.state.imageIndex].title}
+                    </h1>
+                </div>
+                <div className="Content">
+                    <h2>
+                        {images[this.state.imageIndex].subtitle}
+                    </h2>
+                </div>
+            </div>
+            <div className="arrow">
+                <ArrowForwardIosIcon onClick={nextImage}/>
+            </div>
+        </div>
+    )
+  }
 }
 
 export default Carousel;
